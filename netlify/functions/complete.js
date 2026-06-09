2d9dbf97e7a70b5b3d779218e3eca2efd9e74bc1
tanahin <batikperca.jogja@gmail.com>
2026-06-09 18:28:38 +0700

    Update complete.js

--- netlify/functions/complete.js
+++ netlify/functions/complete.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
 
+export default async (req) => {
+  const { paymentId, txid } = await req.json();
+  
+  await axios.post(
+    `https://api.minepi.com/v2/payments/${paymentId}/complete`,
+    { txid },
+    { headers: { 'Authorization': `Key ${process.env.PI_API_KEY}` } }
+  );
+  
+  return new Response('Payment completed');
+};
